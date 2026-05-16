bc873f51fb36f0d63510a6d5cd99aff29c3e24a2
André <andre@andrade.inf.br>
2026-05-16 16:31:44 -0300

    chore(state): configure TanStack Query and Zustand

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/shared/ui/button';
+
 export default function App() {
   return (
     <main className="min-h-screen bg-background text-foreground">
       <div className="p-6">
         <h1 className="text-3xl font-bold">CineDash</h1>
+
+        <Button>Teste</Button>
       </div>
     </main>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app/App';
+import { AppProviders } from './app/providers/app-providers';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </StrictMode>,
 );
