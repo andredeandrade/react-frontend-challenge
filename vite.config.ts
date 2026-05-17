3b6fdfed8a88f9d0920d704f7b24c34831181877
André <andre@andrade.inf.br>
2026-05-16 21:45:28 -0300

    test(setup): configure Vitest and React Testing Library

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
